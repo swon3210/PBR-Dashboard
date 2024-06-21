b9be5d29e3a4d0a1344dbe2e91145fc841a35234
SONG WON LEE <songwonlee@SONGuiMcBookPro.kornet>
2024-06-22 00:04:12 +0900

    데이터 크롤링 API 추가

--- src/app/api/crawling/route.ts
+++ src/app/api/crawling/route.ts
@@ -1,14 +1,16 @@
-export async function GET() {
-  const today = new Date();
-  const oneWeekAgo = new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7);
-  const todayString = `${today.getFullYear()}${String(today.getMonth() + 1).padStart(2, '0')}${today.getDate()}`;
+// 출처
+// http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020502
 
-  const rangeStartString = todayString;
-  const rangeEndString = `${oneWeekAgo.getFullYear()}${String(oneWeekAgo.getMonth() + 1).padStart(
-    2,
-    '0'
-  )}${oneWeekAgo.getDate()}`;
-  // ----------------------------------- 크롤링
+import dayjs from 'dayjs';
+
+export async function GET(request: Request) {
+  // YYYY-MM-DD 의 형식으로 요청이 들어옴
+  const targetDateString = new URL(request.url).searchParams.get('date')?.split('-').join('') ?? '';
+
+  const today = dayjs();
+  const oneWeekAgo = dayjs().subtract(1, 'week');
+  const todayString = today.format('YYYYMMDD');
+  const oneWeekAgoString = oneWeekAgo.format('YYYYMMDD');
 
   let data = {};
 
@@ -24,7 +26,7 @@ export async function GET() {
         Referer: 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020502',
         'Referrer-Policy': 'strict-origin-when-cross-origin',
       },
-      body: `bld=dbms/MDC/STAT/standard/MDCSTAT03501&locale=ko_KR&searchType=1&mktId=STK&trdDd=${todayString}&tboxisuCd_finder_stkisu0_0=024110%2F%EA%B8%B0%EC%97%85%EC%9D%80%ED%96%89&isuCd=KR7024110009&isuCd2=KR7024110009&codeNmisuCd_finder_stkisu0_0=%EA%B8%B0%EC%97%85%EC%9D%80%ED%96%89&param1isuCd_finder_stkisu0_0=ALL&strtDd=${rangeStartString}&endDd=${rangeEndString}&csvxls_isNo=false`,
+      body: `bld=dbms/MDC/STAT/standard/MDCSTAT03501&locale=ko_KR&searchType=1&mktId=STK&trdDd=${targetDateString}&tboxisuCd_finder_stkisu0_0=024110%2F%EA%B8%B0%EC%97%85%EC%9D%80%ED%96%89&isuCd=KR7024110009&isuCd2=KR7024110009&codeNmisuCd_finder_stkisu0_0=%EA%B8%B0%EC%97%85%EC%9D%80%ED%96%89&param1isuCd_finder_stkisu0_0=ALL&strtDd=${todayString}&endDd=${oneWeekAgoString}&csvxls_isNo=false`,
       method: 'POST',
     });
 
